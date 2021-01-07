c61ba50acf1b688a3228d8dbbc5d8ef6d16f9162
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-07 22:00:10 +0100

    :white_check_mark:  update tests"

--- __tests__/Spacer.test.tsx
+++ __tests__/Spacer.test.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Text } from 'react-native'
 import { render } from '@testing-library/react-native'
 
-import { Spacer } from '../src'
+import { Spacer, LayoutContextProvider } from '../src'
 
 describe('Spacer', () => {
-  it('should render component without provider', () => {
+  it('should render component without context provider', () => {
     const { toJSON } = render(
       <>
         <Text>Hello</Text>
@@ -19,5 +19,57 @@ describe('Spacer', () => {
     expect(toJSON()).toMatchSnapshot()
   })
 
-  it.skip('should render component with provider', () => {})
+  it('should render component with custom styles', () => {
+    const { toJSON } = render(
+      <>
+        <Text>Hello</Text>
+
+        <Spacer style={{ backgroundColor: 'orange' }} />
+
+        <Text>World</Text>
+      </>
+    )
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  it('should render component with context provider', () => {
+    const Component = () => (
+      <>
+        <Text>Hello</Text>
+
+        <Spacer />
+
+        <Text>World</Text>
+      </>
+    )
+
+    const { toJSON } = render(
+      <LayoutContextProvider>
+        <Component />
+      </LayoutContextProvider>
+    )
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  it('should render component with debug mode enabled', () => {
+    const Component = () => (
+      <>
+        <Text>Hello</Text>
+
+        <Spacer />
+
+        <Text>World</Text>
+      </>
+    )
+
+    const { toJSON } = render(
+      <LayoutContextProvider debug>
+        <Component />
+      </LayoutContextProvider>
+    )
+
+    expect(toJSON()).toMatchSnapshot()
+  })
 })
--- __tests__/Stack.test.tsx
+++ __tests__/Stack.test.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { Text } from 'react-native'
 import { render } from '@testing-library/react-native'
 
-import { Stack } from '../src'
+import { Stack, LayoutContextProvider } from '../src'
+
+const BareComponent = () => (
+  <Stack>
+    <Text>Hello</Text>
+
+    <Text>World</Text>
+  </Stack>
+)
 
 describe('Stack', () => {
-  it('should render component matching snapshot', () => {
+  it('should render component without context provider', () => {
     const { toJSON } = render(
       <Stack spaces={20} padding={10}>
         <Text>Hello</Text>
@@ -16,4 +24,65 @@ describe('Stack', () => {
 
     expect(toJSON()).toMatchSnapshot()
   })
+
+  it('should render component with custom styles', () => {
+    const { toJSON } = render(
+      <Stack
+        spaces={20}
+        padding={10}
+        style={{ backgroundColor: 'green', padding: 5 }}
+      >
+        <Text>Hello</Text>
+
+        <Text>World</Text>
+      </Stack>
+    )
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  it('should render component with context provider', () => {
+    const { toJSON } = render(
+      <LayoutContextProvider spaces={15} padding={30}>
+        <BareComponent />
+      </LayoutContextProvider>
+    )
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  it('should render component with debug mode enabled', () => {
+    const { toJSON } = render(
+      <LayoutContextProvider spaces={15} padding={30} debug>
+        <BareComponent />
+      </LayoutContextProvider>
+    )
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  it('should overwrite global options', () => {
+    const Component = () => (
+      <Stack spaces={12} padding={16}>
+        <Text>Hello</Text>
+
+        <Stack spaces={5}>
+          <Text>World</Text>
+        </Stack>
+      </Stack>
+    )
+
+    const { toJSON } = render(
+      <LayoutContextProvider spaces={15} padding={30}>
+        <Component />
+      </LayoutContextProvider>
+    )
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  // TODO different padding values
+  // it.each()
+
+  // TODO horizontal stack (flexDirection row)
 })
