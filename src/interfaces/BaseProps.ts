386743713fd7f0bd99c523c96cb737fb1fb03503
Jakub Biesiada <biesiadajakub@icloud.com>
2021-02-18 17:25:12 +0100

    feat: ignore null

--- src/interfaces/BaseProps.ts
+++ src/interfaces/BaseProps.ts
@@ -3,4 +3,5 @@ import type { SingleSize, Padding } from '../types/Padding'
 export interface BaseProps {
   readonly spaces?: SingleSize
   readonly padding?: Padding
+  readonly omitNull?: boolean
 }
