5969e4ab9f13dc96b7ddded977b50a697f46e7c1
Jakub Biesiada <biesiadajakub@icloud.com>
2021-02-20 18:58:42 +0100

    :bug:  fix debug colors for padding

--- src/helpers/preparePaddings.ts
+++ src/helpers/preparePaddings.ts
@@ -2,7 +2,7 @@ import randomColor from 'randomcolor'
 
 import type { Padding } from '../types/Padding'
 
-export const preparePaddings = (paddings: Padding) => {
+export const preparePaddings = (paddings: Padding, debug: boolean) => {
   const color = randomColor()
 
   if (typeof paddings === 'number' || typeof paddings === 'string') {
@@ -11,50 +11,62 @@ export const preparePaddings = (paddings: Padding) => {
 
   switch (paddings.length) {
     case 1: {
-      return {
-        padding: paddings[0],
-        borderWidth: paddings[0],
-        borderColor: color,
-      }
+      return debug
+        ? {
+            borderWidth: paddings[0],
+            borderColor: color,
+          }
+        : {
+            padding: paddings[0],
+          }
     }
 
     case 2: {
-      return {
-        paddingVertical: paddings[0],
-        paddingHorizontal: paddings[1],
-        borderTopWidth: paddings[0],
-        borderBottomWidth: paddings[0],
-        borderLeftWidth: paddings[1],
-        borderRigthWidth: paddings[1],
-        borderColor: color,
-      }
+      return debug
+        ? {
+            borderTopWidth: paddings[0],
+            borderBottomWidth: paddings[0],
+            borderLeftWidth: paddings[1],
+            borderRigthWidth: paddings[1],
+            borderColor: color,
+          }
+        : {
+            paddingVertical: paddings[0],
+            paddingHorizontal: paddings[1],
+          }
     }
 
     case 3: {
-      return {
-        paddingTop: paddings[0],
-        borderTopWidth: paddings[0],
-        paddingHorizontal: paddings[1],
-        borderLeftWidth: paddings[1],
-        borderRigthWidth: paddings[1],
-        paddingBottom: paddings[2],
-        borderBottomWidth: paddings[2],
-        borderColor: color,
-      }
+      return debug
+        ? {
+            borderTopWidth: paddings[0],
+            borderLeftWidth: paddings[1],
+            borderRigthWidth: paddings[1],
+            borderBottomWidth: paddings[2],
+            borderColor: color,
+          }
+        : {
+            paddingTop: paddings[0],
+            paddingHorizontal: paddings[1],
+            paddingBottom: paddings[2],
+          }
     }
 
     case 4: {
-      return {
-        paddingTop: paddings[0],
-        borderTopWidth: paddings[0],
-        paddingRight: paddings[1],
-        borderRigthWidth: paddings[1],
-        paddingBottom: paddings[2],
-        borderBottomWidth: paddings[2],
-        paddingLeft: paddings[3],
-        borderLeftWidth: paddings[3],
-        borderColor: color,
-      }
+      return debug
+        ? {
+            borderTopWidth: paddings[0],
+            borderRigthWidth: paddings[1],
+            borderBottomWidth: paddings[2],
+            borderLeftWidth: paddings[3],
+            borderColor: color,
+          }
+        : {
+            paddingTop: paddings[0],
+            paddingRight: paddings[1],
+            paddingBottom: paddings[2],
+            paddingLeft: paddings[3],
+          }
     }
 
     default: {
