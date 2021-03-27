285742c7810dbf495cf1113091317df92a98c356
Jakub Biesiada <biesiadajakub@icloud.com>
2021-03-27 16:20:50 +0100

    chore: update types

--- src/helpers/preparePaddings.ts
+++ src/helpers/preparePaddings.ts
@@ -1,9 +1,10 @@
 import randomColor from 'randomcolor'
 import { ViewStyle } from 'react-native'
 
+import { typeSafePaddings } from './typeSafePaddings'
+
 import type { Padding } from '../types/Padding'
 
-// TODO refactor, any type, add returned type
 export const preparePaddings = (
   paddings: Padding,
   debug?: boolean
@@ -12,69 +13,67 @@ export const preparePaddings = (
 
   if (typeof paddings === 'number' || typeof paddings === 'string') {
     return debug
-      ? { borderWidth: paddings, borderColor: color }
-      : { padding: paddings }
+      ? { borderWidth: typeSafePaddings(paddings), borderColor: color }
+      : { padding: typeSafePaddings(paddings) }
   }
 
   switch (paddings.length) {
     case 1: {
       return debug
         ? {
-            // TODO parseFloat
-            borderWidth: paddings[0],
+            borderWidth: typeSafePaddings(paddings[0]),
             borderColor: color,
-            // padding: 0, // TODO
           }
         : {
-            padding: paddings[0],
+            padding: typeSafePaddings(paddings[0]),
           }
     }
 
     case 2: {
       return debug
         ? {
-            borderTopWidth: paddings[0],
-            borderBottomWidth: paddings[0],
-            borderLeftWidth: paddings[1],
-            borderRightWidth: paddings[1],
+            borderTopWidth: typeSafePaddings(paddings[0]),
+            borderBottomWidth: typeSafePaddings(paddings[0]),
+            borderLeftWidth: typeSafePaddings(paddings[1]),
+            borderRightWidth: typeSafePaddings(paddings[1]),
             borderColor: color,
           }
         : {
-            paddingVertical: paddings[0],
-            paddingHorizontal: paddings[1],
+            paddingVertical: typeSafePaddings(paddings[0]),
+            paddingHorizontal: typeSafePaddings(paddings[1]),
           }
     }
 
     case 3: {
       return debug
         ? {
-            borderTopWidth: paddings[0],
-            borderLeftWidth: paddings[1],
-            borderRightWidth: paddings[1],
-            borderBottomWidth: paddings[2],
+            borderTopWidth: typeSafePaddings(paddings[0]),
+            borderLeftWidth: typeSafePaddings(paddings[1]),
+            borderRightWidth: typeSafePaddings(paddings[1]),
+            borderBottomWidth: typeSafePaddings(paddings[2]),
             borderColor: color,
           }
         : {
-            paddingTop: paddings[0],
-            paddingHorizontal: paddings[1],
-            paddingBottom: paddings[2],
+            paddingTop: typeSafePaddings(paddings[0]),
+            paddingHorizontal: typeSafePaddings(paddings[1]),
+            paddingBottom: typeSafePaddings(paddings[2]),
           }
     }
 
     case 4: {
       return debug
         ? {
-            borderTopWidth: paddings[0],
-            borderRightWidth: paddings[1],
-            borderBottomWidth: paddings[2],
-            borderLeftWidth: paddings[3],
+            borderTopWidth: typeSafePaddings(paddings[0]),
+            borderRightWidth: typeSafePaddings(paddings[1]),
+            borderBottomWidth: typeSafePaddings(paddings[2]),
+            borderLeftWidth: typeSafePaddings(paddings[3]),
             borderColor: color,
           }
         : {
-            paddingTop: paddings[0],
-            paddingRight: paddings[1],
-            paddingBottom: paddings[2],
-            paddingLeft: paddings[3],
+            paddingTop: typeSafePaddings(paddings[0]),
+            paddingRight: typeSafePaddings(paddings[1]),
+            paddingBottom: typeSafePaddings(paddings[2]),
+            paddingLeft: typeSafePaddings(paddings[3]),
           }
     }
 
---
+++ src/helpers/typeSafePaddings.ts
@@ -0,0 +1,9 @@
+import { SingleSize } from '../types/Padding'
+
+export const typeSafePaddings = (padding: SingleSize) => {
+  if (typeof padding === 'string') {
+    return parseFloat(padding)
+  }
+
+  return padding
+}
