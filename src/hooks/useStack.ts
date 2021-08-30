398b305ac3e787371fc2c280579aea94b430170b
Jakub Biesiada <biesiadajakub@icloud.com>
2021-08-30 17:23:32 +0200

    :arrow_up:  bump packages

--- src/hooks/useStack.ts
+++ src/hooks/useStack.ts
@@ -2,5 +2,4 @@ import { useSafeContext } from 'react-safe-context-hooks'
 
 import { LayoutContext } from '../context'
 
-// TODO check if context exists
 export const useStack = () => useSafeContext(LayoutContext)
